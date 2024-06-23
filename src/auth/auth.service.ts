094b73aa2a072a74c729cf3069a628239a4a4eea
Ruslan Moiseyenko <ruscom5@gmail.com>
2024-06-23 10:24:03 +0300

    Authorization finished. added token add/delete

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,18 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+	Injectable,
+	NotFoundException,
+	UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { verify } from 'argon2';
+import { Response } from 'express';
 import { AuthDto } from 'src/auth/dto/auth.dto';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
+	REFRESH_TOKEN_DAYS_TO_EXPIRE = 1;
+	REFRESH_TOKEN_NAME = 'refreshToken';
 	constructor(
 		private jwt: JwtService,
 		private userService: UserService,
@@ -60,5 +67,39 @@ export class AuthService {
 			throw new NotFoundException('Password is wrong');
 		}
 		return user;
+	}
+
+	addRefreshTokenToResponse(res: Response, refreshToken: string) {
+		const expiresIn = new Date();
+		expiresIn.setDate(expiresIn.getDate() + this.REFRESH_TOKEN_DAYS_TO_EXPIRE);
+		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+			httpOnly: true,
+			domain: 'localhost', // change this to your domain
+			secure: true,
+			sameSite: 'none', //better to use lux in production
+			expires: expiresIn,
+		});
+	}
+
+	removeRefreshTokenForResponse(res: Response) {
+		res.cookie(this.REFRESH_TOKEN_NAME, '', {
+			httpOnly: true,
+			domain: 'localhost', // change this to your domain
+			secure: true,
+			sameSite: 'none', //better to use lux in production
+			expires: new Date(0),
+		});
+	}
+
+	async getNewTokens(refreshToken: string) {
+		const result = await this.jwt.verifyAsync(refreshToken);
+
+		if (!result) throw new UnauthorizedException('Invalid refresh token');
+
+		const { password, ...user } = await this.userService.getById(result.id);
+
+		const tokens = this.issueToken(user.id);
+
+		return { user, ...tokens };
 	}
 }
