97434aba687f584c83af32c355e29270dbf84037
Ruslan Moiseyenko <ruscom5@gmail.com>
2024-06-30 21:49:07 +0300

    added timers and rounds

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from 'src/task/task.module';
 import { TimeBlockModule } from 'src/time-block/time-block.module';
+import { PomodoroModule } from 'src/pomodoro/pomodoro.module';
 
 @Module({
 	imports: [
@@ -13,6 +14,7 @@ import { TimeBlockModule } from 'src/time-block/time-block.module';
 		UserModule,
 		TaskModule,
 		TimeBlockModule,
+		PomodoroModule,
 		ConfigModule.forRoot(),
 	],
 	controllers: [AppController],
